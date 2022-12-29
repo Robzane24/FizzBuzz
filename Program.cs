46eeeb0085b622ed82aa1b77efce7edfeb874c33
Robert Zane <rob.zane24@gmail.com>
2022-12-29 13:29:56 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FIzzBuzz
+{
+    internal class Program
+    {
+        public static string FizzBuzz(int number)
+        {
+            var answer = "";
+
+            if (number % 3 == 0)
+            {
+                answer = "fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                answer = "buzz";
+            }
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                answer = "fizzbuzz";
+            }
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzBuzz(10));
+        }
+        
+    }
+        
+}
